f068789d7e31d92e126a2da5800441262f456d5e
Zacharias Mitzelos <zmitzie@users.noreply.github.com>
2024-09-08 17:06:41 +0200

    Add tests for getReferreralBpsFor in ReferralManager

--- test/ReferralManager.test.ts
+++ test/ReferralManager.test.ts
@@ -19,8 +19,8 @@ describe("BuzzVault Tests", () => {
     let referralManager: Contract;
     let tx: any;
 
-    const directRefFeeBps = 1500;
-    const indirectRefFeeBps = 100;
+    const directRefFeeBps = 1500; // 15% of protocol fee
+    const indirectRefFeeBps = 100; // fixed 1%
     const payoutThreshold = 0;
     let validUntil: number;
 
@@ -128,6 +128,33 @@ describe("BuzzVault Tests", () => {
             });
             it("should emit an IndirectReferralSet event", async () => {
                 await expect(tx).to.emit(referralManager, "IndirectReferralSet");
+            });
+        });
+    });
+    describe("getReferreralBpsFor", () => {
+        beforeEach(async () => {
+            tx = await vault.connect(ownerSigner).buy(token.address, ethers.utils.parseEther("0.001"), user1Signer.address, {
+                value: ethers.utils.parseEther("0.01"),
+            });
+        });
+        it("should return the directFee", async () => {
+            expect(await referralManager.getReferreralBpsFor(ownerSigner.address)).to.be.equal(directRefFeeBps);
+        });
+        it("should return 0 if past the validUntil date", async () => {
+            await helpers.time.increase(ONE_YEAR_IN_SECS);
+            expect(await referralManager.getReferreralBpsFor(ownerSigner.address)).to.be.equal(0);
+        });
+        it("should return 0 if the user doesn't have any referrals", async () => {
+            expect(await referralManager.getReferreralBpsFor(user1Signer.address)).to.be.equal(0);
+        });
+        describe("getReferreralBpsFor - indirect referral", () => {
+            beforeEach(async () => {
+                await vault.connect(user2Signer).buy(token.address, ethers.utils.parseEther("0.001"), ownerSigner.address, {
+                    value: ethers.utils.parseEther("0.01"),
+                });
+            });
+            it("should return the directFee + indirect fee", async () => {
+                expect(await referralManager.getReferreralBpsFor(user2Signer.address)).to.be.equal(directRefFeeBps + indirectRefFeeBps);
             });
         });
     });

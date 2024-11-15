1d53aaea2b8610b80e378628d1b8ec11aaae071d
Zacharias Mitzelos <zmitzie@users.noreply.github.com>
2024-11-15 13:58:35 +0700

    Update deployment script to incorporate latest changes

--- scripts/testing/deployAll.ts
+++ scripts/testing/deployAll.ts
@@ -9,6 +9,7 @@ const crocQueryAddress = "0x8685CE9Db06D40CBa73e3d09e6868FE476B5dC89";
 const wberaHoneyLpToken = "0xd28d852cbcc68dcec922f6d5c7a8185dbaa104b7";
 const create3Address = "0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1";
 const crocSwapDex = "0xAB827b1Cc3535A9e549EE387A6E9C3F02F481B49";
+const wberaAddress = "0x7507c1dc16935B82698e4C63f2746A2fCf994dF8";
 
 // protocol fee is hardcoded in vaults
 
@@ -16,6 +17,8 @@ const crocSwapDex = "0xAB827b1Cc3535A9e549EE387A6E9C3F02F481B49";
 const directRefFeeBps = 1500; // 15% of protocol fee
 const indirectRefFeeBps = 100; // fixed 1%
 const listingFee = ethers.utils.parseEther("0.002");
+const tradingFeeBps = 100; // 1% trading fee
+const migrationFeeBps = 420; // 4.2% migration fee
 const payoutThreshold = 0;
 const validUntil = Math.floor(Date.now() / 1000) + ONE_YEAR_IN_SECS;
 
@@ -37,9 +40,13 @@ async function main() {
     const bexPriceDecoder = await BexPriceDecoder.deploy(wberaHoneyLpToken, crocQueryAddress);
     console.log("BexPriceDecoder deployed to:", bexPriceDecoder.address);
 
+    // Deploy FeeManager
+    const FeeManager = await ethers.getContractFactory("FeeManager");
+    const feeManager = await FeeManager.deploy(feeRecipient, tradingFeeBps, listingFee, migrationFeeBps);
+
     // Deploy ReferralManager
     const ReferralManager = await ethers.getContractFactory("ReferralManager");
-    const referralManager = await ReferralManager.deploy(directRefFeeBps, indirectRefFeeBps, validUntil, payoutThreshold);
+    const referralManager = await ReferralManager.deploy(directRefFeeBps, indirectRefFeeBps, validUntil, [wberaAddress], [payoutThreshold]);
     console.log("ReferralManager deployed to:", referralManager.address);
 
     // Deploy Factory via Create3
@@ -48,16 +55,10 @@ async function main() {
     const factory = new ethers.ContractFactory(abi, factoryBytecode);
     const creationCode = factory.bytecode;
     // change salt for each new deployment
-    const salt = "0xa0000000000000000000000000000379315e9a9d553690c4e61479c25f29552b";
+    const salt = "0x080000000000000000000000000023ad609373a0f37571d4bb90f7ce6521626d";
     const packedBytecode = ethers.utils.solidityPack(
         ["bytes", "bytes"],
-        [
-            creationCode,
-            ethers.utils.defaultAbiCoder.encode(
-                ["address", "address", "address", "uint256"],
-                [deployerAddress, create3Address, feeRecipient, listingFee]
-            ),
-        ]
+        [creationCode, ethers.utils.defaultAbiCoder.encode(["address", "address", "address"], [deployerAddress, create3Address, feeManager.address])]
     );
     const create3FactoryContract = new ethers.Contract(create3Address, DEPLOY_ABI, deployer);
     const tx = await create3FactoryContract.deploy(salt, packedBytecode);
@@ -85,17 +86,21 @@ async function main() {
     // Deploy Exponential Vault
     const ExpVault = await ethers.getContractFactory("BuzzVaultExponential");
     const expVault = await ExpVault.deploy(
-        feeRecipient,
+        feeManager.address,
         factoryInstance.address,
         referralManager.address,
         bexPriceDecoder.address,
-        bexLiquidityManager.address
+        bexLiquidityManager.address,
+        wberaAddress
     );
     console.log("Exponential Vault deployed to:", expVault.address);
 
     // Admin: Set Vault in the ReferralManager
     // await referralManager.setWhitelistedVault(vault.address, true);
     await referralManager.setWhitelistedVault(expVault.address, true);
+
+    // Admin: Whitelist base token in Factory
+    await factoryInstance.setAllowedBaseToken(wberaAddress, true);
 
     // Admin: Set Vault as the factory's vault & enable token creation
     // await factoryInstance.setVault(vault.address, true);

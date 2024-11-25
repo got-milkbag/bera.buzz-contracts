8b5eabb3edce5662c80a59c11dd9e40a4e721542
0xnexusflip <0xnexusflip@gmail.com>
2024-11-25 21:53:24 +0700

    add amount after pr fee to quote

--- contracts/BuzzVaultExponential.sol
+++ contracts/BuzzVaultExponential.sol
@@ -52,12 +52,13 @@ contract BuzzVaultExponential is BuzzVault {
         if (tokenBalance == 0 && baseBalance == 0) revert BuzzVault_UnknownToken();
 
         uint256 circulatingSupply = TOTAL_MINTED_SUPPLY - tokenBalance;
+        uint256 amountAfterFee = amount - feeManager.quoteTradingFee(amount);
 
         if (isBuyOrder) {
-            (amountOut, pricePerToken, pricePerBase) = _calculateBuyPrice(circulatingSupply, amount, CURVE_ALPHA, CURVE_BETA);
+            (amountOut, pricePerToken, pricePerBase) = _calculateBuyPrice(circulatingSupply, amountAfterFee, CURVE_ALPHA, CURVE_BETA);
             if (amountOut > tokenBalance) revert BuzzVault_InvalidReserves();
         } else {
-            (amountOut, pricePerToken, pricePerBase) = _calculateSellPrice(circulatingSupply, amount, CURVE_ALPHA, CURVE_BETA);
+            (amountOut, pricePerToken, pricePerBase) = _calculateSellPrice(circulatingSupply, amountAfterFee, CURVE_ALPHA, CURVE_BETA);
             if (amountOut > baseBalance) revert BuzzVault_InvalidReserves();
         }
     }

b70c70126da3ff7b24376591d5147c45557c9f88
Zacharias Mitzelos <zmitzie@users.noreply.github.com>
2024-12-04 18:30:27 +0100

    Update highlights manager, remove queue logic, add cool down, bookedUntil with reverts

--- contracts/HighlightsManager.sol
+++ contracts/HighlightsManager.sol
@@ -19,15 +19,21 @@ contract HighlightsManager is Ownable, ReentrancyGuard {
     error HighlightsManager_TreasuryZeroAddress();
     /// @notice Error thrown when the transfer in native currency fails
     error HighlightsManager_EthTransferFailed();
+    /// @notice Error thrown when the slot is already occupied
+    error HighlightsManager_SlotOccupied();
+    /// @notice Error thrown when the token is within the cool down period
+    error HighlightsManager_TokenWithinCoolDown();
 
     /// @notice Event emitted when a token is highlighted
-    event TokenHighlighted(address indexed token, address indexed buyer, uint256 startAt, uint256 duration, uint256 bookedUntil);
+    event TokenHighlighted(address indexed token, address indexed buyer, uint256 duration, uint256 bookedUntil);
     /// @notice Event emitted when the treasury address is set
     event TreasurySet(address indexed treasury);
     /// @notice Event emitted when the base fee is set
     event BaseFeeSet(uint256 baseFeePerSecond);
     /// @notice Event emitted when the hard cap is set
     event HardCapSet(uint256 hardCap);
+    /// @notice Event emitted when the cool down period is set
+    event CoolDownPeriodSet(uint256 coolDownPeriod);
 
     /// @notice The minimum duration allowed in seconds
     uint256 public constant MIN_DURATION = 60; // 60 = 1 minute
@@ -35,6 +41,8 @@ contract HighlightsManager is Ownable, ReentrancyGuard {
     uint256 public constant EXP_THRESHOLD = 600; // 600 = 10 minutes
     /// @notice The maximum duration allowed in seconds
     uint256 public hardCap;
+    /// @notice The cool down period for a token in seconds
+    uint256 public coolDownPeriod;
     /// @notice The base fee per second to charge in wei
     uint256 public baseFeePerSecond;
     /// @notice The treasury address where fees are sent
@@ -42,23 +50,27 @@ contract HighlightsManager is Ownable, ReentrancyGuard {
     /// @notice The timestamp when the latest highlight expires
     uint256 public bookedUntil;
 
+    mapping(address => uint256) public tokenCoolDownUntil;
+
     /**
      * @notice Constructor
      * @param _treasury The treasury address where fees are sent
      * @param _hardCap The maximum duration allowed in seconds
      * @param _baseFeePerSecond The base fee per second to charge in wei
      */
-    constructor(address payable _treasury, uint256 _hardCap, uint256 _baseFeePerSecond) {
+    constructor(address payable _treasury, uint256 _hardCap, uint256 _baseFeePerSecond, uint256 _coolDownPeriod) {
         if (_hardCap < MIN_DURATION) revert HighlightsManager_HardCapBelowMinimumDuration();
         if (_treasury == address(0)) revert HighlightsManager_TreasuryZeroAddress();
 
         treasury = _treasury;
         hardCap = _hardCap;
         baseFeePerSecond = _baseFeePerSecond;
+        coolDownPeriod = _coolDownPeriod;
 
         emit TreasurySet(_treasury);
         emit HardCapSet(_hardCap);
         emit BaseFeeSet(_baseFeePerSecond);
+        emit CoolDownPeriodSet(_coolDownPeriod);
     }
 
     /**
@@ -67,21 +79,18 @@ contract HighlightsManager is Ownable, ReentrancyGuard {
      * @param duration The duration in seconds
      */
     function highlightToken(address token, uint256 duration) external payable nonReentrant {
+        if (bookedUntil > block.timestamp) revert HighlightsManager_SlotOccupied();
+        if (tokenCoolDownUntil[token] > block.timestamp) revert HighlightsManager_TokenWithinCoolDown();
+
         uint256 fee = quote(duration);
         if (msg.value != fee) revert HighlightsManager_InsufficientFee();
-        uint256 startAt;
-        if (bookedUntil > block.timestamp) {
-            startAt = bookedUntil + 1;
-            bookedUntil += duration;
-        } else {
-            startAt = block.timestamp;
-            bookedUntil = block.timestamp + duration;
-        }
 
+        bookedUntil = block.timestamp + duration;
+        tokenCoolDownUntil[token] = block.timestamp + coolDownPeriod;
         (bool success, ) = treasury.call{value: fee}("");
         if (!success) revert HighlightsManager_EthTransferFailed();
 
-        emit TokenHighlighted(token, msg.sender, startAt, duration, bookedUntil);
+        emit TokenHighlighted(token, msg.sender, duration, bookedUntil);
     }
 
     /**
@@ -141,5 +150,10 @@ contract HighlightsManager is Ownable, ReentrancyGuard {
     function setBaseFee(uint256 _baseFeePerSecond) external onlyOwner {
         baseFeePerSecond = _baseFeePerSecond;
         emit BaseFeeSet(_baseFeePerSecond);
+    }
+
+    function setCoolDownPeriod(uint256 _coolDownPeriod) external onlyOwner {
+        coolDownPeriod = _coolDownPeriod;
+        emit CoolDownPeriodSet(_coolDownPeriod);
     }
 }

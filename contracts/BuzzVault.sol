dab386c4299b07e46baee6e29773f55666555140
0xnexusflip <0xnexusflip@gmail.com>
2024-12-23 07:41:55 +0300

    fix createPoolAndAdd amounts

--- contracts/BuzzVault.sol
+++ contracts/BuzzVault.sol
@@ -299,7 +299,7 @@ abstract contract BuzzVault is Ownable, Pausable, ReentrancyGuard, IBuzzVault {
         IERC20(token).safeApprove(address(liquidityManager), tokenBalance);
         IERC20(baseToken).safeApprove(address(liquidityManager), netBaseAmount);
 
-        liquidityManager.createPoolAndAdd(token, baseToken, tokenBalance, netBaseAmount);
+        liquidityManager.createPoolAndAdd(token, baseToken, netBaseAmount, tokenBalance);
  
         // burn any rounding excess
         if (IERC20(token).balanceOf(address(this)) > 0) {
